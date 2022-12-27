d4c47e15d8bc9a443702f127545db04b41ffa7df
Toujour <24385425+aorosora@users.noreply.github.com>
2022-12-27 21:22:59 +0800

    update

--- index.php
+++ index.php
@@ -11,27 +11,29 @@ $weekarray = array("日","一","二","三","四","五","六");
 //ua
 $parser = Parser::create();
 $result = $parser->parse($ua);
-$os = $result->os->toString(); // Mac OS X
-$browser = $result->device->family.'-'.$result->ua->family;// Safari 6.0.2 
+$os = $result->os->toString();
+//$browser = $result->device->family.'-'.$result->ua->family;
+$browser = $result->ua->family;
 //地址
-$data = json_decode(curl_get('http://ip.taobao.com/outGetIpInfo?ip='.$ip.'&accessKey=alibaba-inc'), true);
+$data = json_decode(curl_get('https://ip.taobao.com/outGetIpInfo?ip='.$ip.'&accessKey=alibaba-inc'), true);
 $country = $data['data']['country']; 
-$region = $data['data']['city']; 
-$adcode = $data['site']['adcode']; 
+$region = $data['data']['region']; 
+$city = $data['data']['city']; 
+//$adcode = $data['site']['adcode']; 
 //温度
-$data_w = json_decode(curl_get('http://wthrcdn.etouch.cn/weather_mini?city='.$region),true);
-$weather = substr($data_w['data']['forecast'][0]['high'],7);
-$temperature = substr($data_w['data']['forecast'][0]['low'],7);
+//$data_w = json_decode(curl_get('http://wthrcdn.etouch.cn/weather_mini?city='.$region),true);
+//$weather = substr($data_w['data']['forecast'][0]['high'],7);
+//$temperature = substr($data_w['data']['forecast'][0]['low'],7);
 //定义颜色
 $black = ImageColorAllocate($im, 0,0,0);//定义黑色的值
-$red = ImageColorAllocate($im, 255,0,0);//红色
+$pink = ImageColorAllocate($im, 255,98,221);//粉色
 $font = 'msyh.ttf';//加载字体
 //输出
-imagettftext($im, 16, 0, 10, 40, $red, $font,'欢迎您来自'.$country.'-'.$region.'的朋友');
-imagettftext($im, 16, 0, 10, 72, $red, $font, '今天是'.date('Y年n月j日').' 星期'.$weekarray[date("w")]);//当前时间添加到图片
-imagettftext($im, 16, 0, 10, 104, $red, $font,'您的IP是:'.$ip.'  '.$weather."-".$temperature);//ip和温度
-imagettftext($im, 16, 0, 10, 140, $red, $font,'您使用的是'.$os.'操作系统');
-imagettftext($im, 16, 0, 10, 175, $red, $font,'您使用的是'.$browser);
+imagettftext($im, 16, 0, 10, 40, $pink, $font,'欢迎您，来自'.$country.''.$region.''.$city.'的朋友');
+imagettftext($im, 16, 0, 10, 72, $pink, $font, '现在是'.date('Y年n月j日').' 星期'.$weekarray[date("w")]);//当前时间添加到图片
+imagettftext($im, 16, 0, 10, 104, $pink, $font,'您当前IP是'.$ip);//ip
+imagettftext($im, 16, 0, 10, 140, $pink, $font,'您使用的是'.$os.'操作系统');
+imagettftext($im, 16, 0, 10, 175, $pink, $font,'您使用的是'.$browser.'浏览器');
 imagettftext($im, 13, 0, 10, 200, $black, $font,$get); 
 ImageGif($im);
 ImageDestroy($im);
